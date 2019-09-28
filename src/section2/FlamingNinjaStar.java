441635ca7574b110cb8ccbf04906be77646e1fc0
League Student <league@iMac-11.attlocal.net>
2019-09-28 12:56:03 -0700

    oceanbuttface amd ChunkyBoi

--- src/section2/FlamingNinjaStar.java
+++ src/section2/FlamingNinjaStar.java
@@ -10,35 +10,39 @@ public class FlamingNinjaStar {
 		int flameSize = 200;		//the length of the flaming arms
 		
 		// 1. Make a new robot, and set it's pen down.
-
+Robot yeet = new Robot();
+yeet.penDown();
 		// *14. Use the methods setX and setY to move the ninja star into the center of the screen
-		
+		yeet.setX(500);
+		yeet.setY(250);
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
 
 		// 12. Set the robot speed to 100
-
+yeet.setSpeed(500);
 		// 13. Use a for loop to repeat all of the code below 25 times
-
+for(int i=0; i<25; i++) {
 			// 2. Turn the robot 1/8 of a circle
-		
+		yeet.turn(45);
 			// 3. Move the robot 64 pixels
-
+		yeet.move(64);
 			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+			yeet.setPenColor(255, 0, 0);
+		yeet.turn(-40);
 			// 5. Move the robot the distance in the variable flameSize
-			
+			yeet.move(flameSize);
 			// 6. Turn the robot 170 degrees
-			
+			yeet.turn(170);
 			// 7. Move the robot the distance in the variable flameSize (again)
-			
+			yeet.move(flameSize);
 			// 8. Turn the robot 64 degrees to the right
-			
+			yeet.setPenColor(0, 0, 0);
+			yeet.turn(64);
 			// 9. Move the robot the distance in the variable baseSize
-			
+			yeet.move(baseSize);
 			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
 			
 			// 11. Color your ninja star like Figure 2.
-		
+}
 	}
 
 }
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,63 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot oceanbuttface = new Robot();
+	oceanbuttface.hide();
+	oceanbuttface.penDown();
+	oceanbuttface.setPenColor(248, 24, 49);
+	oceanbuttface.setSpeed(200);
+	oceanbuttface.move(100);
+	oceanbuttface.turn(90);
+	oceanbuttface.move(100);
+	oceanbuttface.turn(90);
+	oceanbuttface.move(100);
+	oceanbuttface.turn(90);
+	oceanbuttface.move(100);
+	oceanbuttface.turn(90);
+	oceanbuttface.move(100);
+	oceanbuttface.turn(45);
+	oceanbuttface.move(50);
+	oceanbuttface.turn(45);
+	oceanbuttface.move(100);
+	oceanbuttface.turn(135);
+	oceanbuttface.move(50);
+	oceanbuttface.turn(-45);
+	oceanbuttface.move(100);
+	oceanbuttface.turn(-135);
+	oceanbuttface.move(50);
+	oceanbuttface.turn(-45);
+	oceanbuttface.move(100);
+	oceanbuttface.penUp();
+	oceanbuttface.move(50);
 	
-		
-		
+	Robot chunkyBoi = new Robot();
+	chunkyBoi.penDown();
+	chunkyBoi.setSpeed(420);
+	for(int i=0; i<3; i++) {
+		chunkyBoi.move(68);
+		chunkyBoi.turn(120);
+	}
+	chunkyBoi.hide();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 }
