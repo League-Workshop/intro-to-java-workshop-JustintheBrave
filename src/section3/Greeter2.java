ad82cb365e359eec78ed70e35976ac2b57f87b02
League Student <league@iMac-11.attlocal.net>
2019-10-05 12:53:04 -0700

    Stephan Hawking
    :(

---
+++ src/section3/Greeter2.java
@@ -0,0 +1,13 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Greeter2 {
+//im kool
+public static void main(String[] args) {
+	String name = JOptionPane.showInputDialog("what's your name?");
+	JOptionPane.showMessageDialog(null, name + ", ur fat");
+}
+	}
+
+
